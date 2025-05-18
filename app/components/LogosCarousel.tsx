10fe56191d62e3cd789b27ab65027d6f7f260376
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-05-18 17:24:17 +0100

    Fixed cards scale

--- app/components/LogosCarousel.tsx
+++ app/components/LogosCarousel.tsx
@@ -1,24 +1,37 @@
 import { companyicons } from "~/assets/images/companies";
-import { motion } from "motion/react";
+import { motion, type Variants } from "motion/react";
 
 const LogosCarousel: React.FC = () => {
+  const container: Variants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
   return (
     <motion.div
       className={`grid sm:grid-cols-7 grid-cols-1 w-full h-fit card-grid`}
-      variants={{
-        show: {
-          transition: {
-            staggerChildren: 0.15,
-          },
-        },
-      }}
+      variants={container}
+      initial={"hidden"}
+      whileInView={"show"}
     >
-      {companyicons.map((logo, i) => (
-        <div key={i}>
+      {companyicons.map((logo, i) => {
+        const { scale, zIndex } = getStyle(i);
+        return (
           <motion.div
-            initial={{ y: 50, opacity: 0 }}
-            whileInView={{ y: 0, opacity: 1 }}
+            key={i}
+            variants={{
+              hidden: { opacity: 0, y: 50, scale },
+              show: { opacity: 1, y: 0, scale },
+            }}
+            initial={"hidden"}
+            whileInView={"show"}
             transition={{ duration: 0.5, ease: "easeIn" }}
+            style={{ zIndex }}
             className="sm:w-full w-[180px] shadow-[_-1rem_0_3rem_rgba(0,0,0,0.25)] mx-auto sm:aspect-[2.5/3] aspect-[5/1] flex justify-center items-center p-3 lg:rounded-2xl rounded-xl bg-[#faf9f6]"
           >
             <picture>
@@ -34,9 +47,24 @@ const LogosCarousel: React.FC = () => {
               />
             </picture>
           </motion.div>
-        </div>
-      ))}
+        );
+      })}
     </motion.div>
   );
 };
 export default LogosCarousel;
+
+const getStyle = (i: number) => {
+  switch (i) {
+    case 3:
+      return { scale: 1.14, zIndex: 3 };
+    case 2:
+    case 4:
+      return { scale: 1.08, zIndex: 2 };
+    case 1:
+    case 5:
+      return { scale: 1.05, zIndex: 1 };
+    default:
+      return { scale: 1, zIndex: 0 };
+  }
+};
